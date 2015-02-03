15a3cfd46d73b25fc056e92199c9562ee0182232
Gurgen Tumanyan <gurgen@flurry.com>
2015-02-02 22:50:39 -0800

    clean the trash before the run

--- run_ingestion.sh
+++ run_ingestion.sh
@@ -4,4 +4,5 @@ mkdir -p tmp
 mkdir -p logs
 dt=$(date '+%d%m%Y%H%M%S');
 
+hadoop fs -expunge
 cd tmp && nohup ../scripts/create_tables_parallel.sh ../conf/tables.conf ../logs/$dt.out 2> ../logs/$dt.err >../logs/$dt.log & 
