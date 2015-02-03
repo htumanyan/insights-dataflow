f9ac118247aae1081bc0f2efd6313467d9800922
Gurgen Tumanyan <gurgen@flurry.com>
2015-02-02 22:48:46 -0800

    even nicer

--- run_ingestion.sh
+++ run_ingestion.sh
@@ -4,4 +4,4 @@ mkdir -p tmp
 mkdir -p logs
 dt=$(date '+%d%m%Y%H%M%S');
 
-cd tmp && ../scripts/create_tables_parallel.sh ../conf/tables.conf ../logs/$dt.out 
+cd tmp && nohup ../scripts/create_tables_parallel.sh ../conf/tables.conf ../logs/$dt.out 2> ../logs/$dt.err >../logs/$dt.log & 
