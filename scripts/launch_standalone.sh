977ace5a80de4f1185cbb31fd2bb83898723a5d2
G <tumanian@gmail.com>
2015-08-25 01:09:58 +0000

    minor fixes to the hqls

--- scripts/launch_standalone.sh
+++ scripts/launch_standalone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-env = $1
-db = $2 
-current_sharelib=`sudo su -oozie -c "oozie admin -sharelibupdate | grep sharelibDirNew | sed 's/.*oozie\/share\/lib\/\(.*\)/\1/'"`
-sudo su - oozie -c 'oozie job -config oozie/configs/$env/standalone_jobs/$db.properties -run -Dcurrent_sharelib='$current_sharelib
+env=$1
+db=$2 
+current_sharelib=`sudo su - oozie -c "oozie admin -sharelibupdate | grep sharelibDirNew | sed 's/.*oozie\/share\/lib\/\(.*\)/\1/'"`
+sudo su oozie -c 'oozie job -config '`pwd`'/oozie/configs/'$env'/standalone_jobs/'$db'.properties -run -Dcurrent_sharelib='$current_sharelib
