d4ee119fa658331ba0091ccc7069bca61b600c92
Tyler Chapman <tyler@flurry.com>
2015-09-01 14:49:21 -0700

    parameterizing upload and download

--- hql/mmr/mmr_download.sh
+++ hql/mmr/mmr_download.sh
@@ -2,24 +2,24 @@
 
 Today=`date +"%Y%m%d"`
 Yesterday=`date --date='1 day ago' + %Y%m%d`
-Filename='INTS_MMR_OUTPUT_'
+Filename=$1
 Filetype='.csv'
 TodayFile=$Filename$Today$Filetype
-YesterdayFile=$Filename$Yesterday.no_head$Filetype
-Server='mft.manheim.com'
-User='rms_insights_mmr'
-LDirectory='/mnt/resource/'
-EDirectory='Inbox/MMR/'
-Password='rm$instsmm9'
+YesterdayFile=$Filename$Yesterday'.no_head'$Filetype
+Server=$2
+User=$3
+LDirectory=$4
+RDirectory=$5
+Password=$6
 
 ftp -n $Server <<End-Of-Session
 user $User $Password
-cd $EDirectory
+cd $RDirectory
 lcd $LDirectory
 get $TodayFile
 bye
 End-Of-Session
 
-sed '1d' $TodayFile > $Filename$Today.nohead$Filetype
-hdfs dfs -put $Filename$Today.nohead$Filetype /data/database/mmr/
+sed '1d' $TodayFile > $Filename$Today'.nohead'$Filetype
+hdfs dfs -put $Filename$Today'.nohead'$Filetype /data/database/mmr/
 hdfs dfs -rm $YesterdayFile /data/database/mmr/
