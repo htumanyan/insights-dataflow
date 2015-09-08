c12bfe5df6cc2419f7d6a643a3df8c0d8d354938
Gurgen Tumanyan <tumanian@Gurgens-MacBook-Pro.local>
2015-09-08 11:00:19 -0700

    run  the script as rmsadmin

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,8 @@
 NUM_MACHINES=$1
 
 sudo easy_install  quik 
-sudo su - oozie -c '/usr/hdp/current/oozie-server/bin/oozie-setup.sh sharelib create -fs /user/oozie/share/lib/' 
-libpath=`sudo su - oozie -c 'oozie admin -sharelibupdate' | grep sharelibDirNew | sed 's/.*oozie\/share\/lib\/\(.*\)/\1/'` #find out where is the sharelib
+/usr/hdp/current/oozie-server/bin/oozie-setup.sh sharelib create -fs /user/oozie/share/lib/ 
+libpath=`oozie admin -sharelibupdate | grep sharelibDirNew | sed 's/.*oozie\/share\/lib\/\(.*\)/\1/'` #find out where is the sharelib
 python oozie/generators/workflowGenerator.py rpm $NUM_MACHINES conf/rpm_tables.conf oozie/templates/ oozie/workflows/rpm/
 python oozie/generators/workflowGenerator.py psa $NUM_MACHINES conf/psa_tables.conf oozie/templates/ oozie/workflows/psa/
 hadoop fs -copyFromLocal -f hql /user/oozie/share
@@ -14,9 +14,8 @@ hadoop fs -copyFromLocal -f oozie/standalone_jars/* /user/oozie/share/lib
 hadoop fs -copyFromLocal -f oozie/sharelib/*  /user/oozie/share/lib/$libpath/ 
  hadoop fs -copyFromLocal /usr/hdp/current/hive-client/conf/hive-site.xml /tmp/hive-site.xml
  hadoop fs -copyFromLocal /usr/hdp/current/hive-client/conf/hive-site.xml /tmp/oozie-hive-site.xml
-sudo su - oozie -c 'oozie admin -sharelibupdate' #update the sharlib for realz
-sudo su - oozie -c 'oozie admin -shareliblist'|  grep 'hive-0.13'
-if   `sudo su - oozie -c "oozie admin -shareliblist|  grep 'hive-0.13'"`
+oozie admin -sharelibupdate #update the sharlib for real
+if   [ oozie admin -shareliblist|  grep 'hive-0.13']`
 then
 	echo '....sharelib deployed successfully...'
 else
